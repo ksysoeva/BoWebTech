22400031457d5c862d3f278ec8d2dff2f1d55dc0
ksysoeva <145020185+ksysoeva@users.noreply.github.com>
2023-11-06 20:55:44 +0300

    BoWebTech
    BoWebTech

---
+++ js/form.js
@@ -0,0 +1,12 @@
+ function calculateX() {
+            var a = parseFloat(document.getElementById("inputA").value);
+            var x;
+
+            if (a < 62) {
+                x = Math.pow(a, 2) + 4 + 5;
+            } else {
+                x = 1 / (Math.pow(a, 2) + 4 * a + 5);
+            }
+
+            document.getElementById("result").innerHTML = "Результат: " + x;
+        }
