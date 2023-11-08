0c0a6a9d7d534c12d2dbece56eab03045755afe1
ksysoeva <145020185+ksysoeva@users.noreply.github.com>
2023-11-08 13:37:52 +0300

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,4 +1,5 @@
- function calculateX() {
+
+function calculateX() {
             var a = parseFloat(document.getElementById("inputA").value);
             var x;
 
@@ -7,6 +8,30 @@
             } else {
                 x = 1 / (Math.pow(a, 2) + 4 * a + 5);
             }
-
             document.getElementById("result").innerHTML = "Результаты вычислений: " + x;
-        }
+}
+
+function send() {
+        document.getElementsByName('formulation')[0].value = textCondition;
+        document.getElementById('result').innerText.slice(23)= result;
+        document.getElementById("UserEnter").submit();
+}
+
+function verify_send() {
+   verify();
+   send();
+}
+
+
+    let messageText = document.getElementById("result").innerText
+    console.log(messageText)
+    let result;
+    let check = false;
+
+
+
+    const elementcalculateX = document.getElementById("calculateX");
+    elementVerify.addEventListener('click', calculateX);
+
+    const elementSend = document.getElementById("send");
+    elementSend.addEventListener('click', send)
